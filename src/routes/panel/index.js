0d8bfde0ae29f80f1ecaefff53734f795e6ce045
Tyler Twining <twijn@twijn.net>
2022-07-22 20:31:28 -0400

    Updates

---
+++ src/routes/panel/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+export default class Panel extends React.Component {
+    render() {
+        return (
+            <div>Cock</div>
+        );
+    }
+}
